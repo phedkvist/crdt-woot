a55c9df7595449cdcf46afc8e95cac5689c0845f
Pierre Hedkvist <pierre.hedkvist@gmail.com>
2021-02-11 19:21:58 +0100

    Add build to package json

--- client-example/src/App.tsx
+++ client-example/src/App.tsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-// import crdt from 'crdt-woot';
+import CRDT from '../../src/controller';
+import { generateSite } from '../../src/utils';
 import ReactQuill from 'react-quill';
+
 import 'react-quill/dist/quill.snow.css';
 import './App.css';
+
+const { start, end, siteId } = generateSite();
+const editor = new CRDT(start, end, siteId);
 
 function App() {
   const [value, setValue] = useState('');
 
+  const onChange = (content: string, delta: any, source: any) => {
+    setValue(value);
+  };
+
   return (
     <div className="App">
       <div className="editor">
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
+        <ReactQuill theme="snow" value={value} onChange={onChange} />
       </div>
     </div>
   );
